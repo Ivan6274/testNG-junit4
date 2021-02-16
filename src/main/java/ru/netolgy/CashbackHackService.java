4602d790a3199acfea3d87bc2e782914f524a3fa
Ivan6274 <isivanov2011@gmail.com>
2021-02-16 11:33:20 +0300

    Fix commit

--- src/main/java/ru/netolgy/CashbackHackService.java
+++ src/main/java/ru/netolgy/CashbackHackService.java
@@ -4,7 +4,16 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
+        if (amount % boundary == 0) {
+            return 0;
+        }
+        if (amount > 0 && amount < 10000) {
+            return boundary - amount % boundary;
+        }
 
-        return boundary - amount % boundary;
+        if (amount > 100000) {
+            return 0;
+        }
+        return 0;
     }
 }
